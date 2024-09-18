15cda21a976cba206f84416d8bf9ab3eca1d480c
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com.>
2024-09-18 15:58:13 +0530

    create login page

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>CuraConnect</title>
     <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="assets/css/style-common.css" rel="stylesheet" />
   </head>
   <body>
 
---
+++ login.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/style-common.css" />
+  </head>
+  <body>
+
+    <!--header-->
+    <?php include ('includes/header.php'); ?>
+    <!--header end-->
+
+
+    <section class="login-section">
+      <center>
+        <div class="login-block">
+          <h2>Welcome Back!</h2>
+          <p class="sub-text">Login with your details to continue</p>
+          <div class="login-form">
+            <form action="" method="POST">
+              <table border="0" style="margin: 0; padding: 0; width: 60%">
+                <tr>
+                  <td class="label-td">
+                    <label for="useremail" class="form-label">Email: </label>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="label-td">
+                    <input
+                      type="email"
+                      name="useremail"
+                      class="input-text"
+                      placeholder="Email Address"
+                      required
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td class="label-td">
+                    <label for="userpassword" class="form-label"
+                      >Password:
+                    </label>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="label-td">
+                    <input
+                      type="Password"
+                      name="userpassword"
+                      class="input-text"
+                      placeholder="Password"
+                      required
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <br />
+                  </td>
+                </tr>
+                <tr>
+                  <td class="label-td">
+                    <input
+                      type="submit"
+                      value="Login"
+                      class="common-btn login-btn"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <br />
+                    <label for="" class="sub-text" style="font-weight: 280"
+                      >Don't have an account&#63;
+                    </label>
+                    <a href="signup.php" class="hover-link1">Sign Up</a>
+                    <br /><br /><br />
+                  </td>
+                </tr>
+              </table>
+            </form>
+          </div>
+        </div>
+      </center>
+    </section>
+  </body>
+</html>
