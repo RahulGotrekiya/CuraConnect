9c55e8c07995aad04f307ab7c414df627f1ed88c
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com.>
2024-09-20 00:00:19 +0530

    connect the database

---
+++ includes/connection.php
@@ -0,0 +1,19 @@
+<?php
+$db_server = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'curaconnect';
+$conn = '';
+
+try {
+    $conn = mysqli_connect($db_server,
+        $db_user,
+        $db_pass,
+        $db_name);
+} catch (mysqli_sql_exception) {
+    echo 'Could not connected!?';
+}
+if ($conn) {
+    echo 'You are connected!!!';
+}
+?>
